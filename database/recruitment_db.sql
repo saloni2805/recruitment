7e22933650660cf67e7d280a28cc194a0fda43f6
saloni2805 <salonispotdar@gmail.com>
2024-07-29 11:57:12 +0530

    updatedd

--- database/recruitment_db.sql
+++ database/recruitment_db.sql
@@ -1,31 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 28, 2020 at 09:50 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `recruitment_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `application`
---
 
 CREATE TABLE `application` (
   `id` int(30) NOT NULL,
